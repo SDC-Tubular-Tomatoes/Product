1b5b7c1ed794f407390753e0027921181d3dfca7
1998rtran <1998rtran@gmail.com>
2023-05-25 17:27:59 -0700

    updated PostgreSQL schema

--- PostgreSQL.sql
+++ PostgreSQL.sql
@@ -1,22 +1,12 @@
 DROP DATABASE IF EXISTS products_database;
 CREATE DATABASE products_database;
 
-DROP TABLE IF EXISTS products;
-DROP TABLE IF EXISTS relateditems;
 DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS relateditems;
 DROP TABLE IF EXISTS styles;
 DROP TABLE IF EXISTS photos;
 DROP TABLE IF EXISTS inventory;
-
-CREATE TABLE products (
-  product_id INT PRIMARY KEY NOT NULL
-);
-
-CREATE TABLE relateditems (
-  product_id SERIAL PRIMARY KEY NOT NULL,
-  related_ids INT NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES products (product_id)
-);
+DROP TABLE IF EXISTS features;
 
 CREATE TABLE product (
   product_id SERIAL PRIMARY KEY NOT NULL,
@@ -25,30 +15,45 @@ CREATE TABLE product (
   product_description TEXT NOT NULL,
   category VARCHAR(20) NOT NULL,
   default_price INT NOT NULL,
-  features VARCHAR(50)[] NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES products (product_id)
+);
+
+CREATE TABLE relateditems (
+  id SERIAL PRIMARY KEY NOT NULL,
+  product_id INT NOT NULL,
+  related_id INT NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES product (product_id)
 );
 
 CREATE TABLE styles (
   id SERIAL PRIMARY KEY NOT NULL,
   product_id INT NOT NULL,
-  style_id INT NOT NULL,
   style_name VARCHAR(10),
   sale_price INT NOT NULL,
-  FOREIGN KEY (product_id) REFERENCES products (product_id)
+  original_price INT NOT NULL,
+  default_style BOOLEAN NOT NULL,
+  FOREIGN KEY (product_id) REFERENCES product (product_id)
 );
 
 CREATE TABLE photos (
-  photo_id SERIAL PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
+  style_id INT NOT NULL,
   pic_url VARCHAR(200) NOT NULL,
-  thumbnail VARCHAR(200) NOT NULL,
-    FOREIGN KEY (photo_id) REFERENCES styles (id)
+  thumbnail_url VARCHAR(200) NOT NULL,
+    FOREIGN KEY (style_id) REFERENCES styles (id)
 );
 
 CREATE TABLE inventory (
-  sku_id SERIAL PRIMARY KEY NOT NULL,
-  quantity INT NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
+  style_id INT NOT NULL,
   size CHAR(3) NOT NULL,
-    FOREIGN KEY (sku_id) REFERENCES styles (id)
+  quantity INT NOT NULL,
+    FOREIGN KEY (style_id) REFERENCES styles (id)
 );
 
+CREATE TABLE features (
+  id SERIAL PRIMARY KEY NOT NULL,
+  product_id INT NOT NULL,
+  feature VARCHAR(30) NOT NULL,
+  feature_value VARCHAR(30) NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES product (id)
+);
