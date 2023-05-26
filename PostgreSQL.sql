7c2b169053d05707f0549cc6d1af2579ba1c6f7c
1998rtran <1998rtran@gmail.com>
2023-05-25 22:57:06 -0700

    added CSV commands from terminal

--- PostgreSQL.sql
+++ PostgreSQL.sql
@@ -1,20 +1,20 @@
 DROP DATABASE IF EXISTS products_database;
 CREATE DATABASE products_database;
 
-DROP TABLE IF EXISTS product;
-DROP TABLE IF EXISTS relateditems;
-DROP TABLE IF EXISTS styles;
+DROP TABLE IF EXISTS features;
 DROP TABLE IF EXISTS photos;
 DROP TABLE IF EXISTS inventory;
-DROP TABLE IF EXISTS features;
+DROP TABLE IF EXISTS relateditems;
+DROP TABLE IF EXISTS styles;
+DROP TABLE IF EXISTS product;
 
 CREATE TABLE product (
   product_id SERIAL PRIMARY KEY NOT NULL,
   item_name VARCHAR(30) NOT NULL,
-  slogan VARCHAR(100) NOT NULL,
+  slogan VARCHAR(255) NOT NULL,
   product_description TEXT NOT NULL,
   category VARCHAR(20) NOT NULL,
-  default_price INT NOT NULL,
+  default_price INT NOT NULL
 );
 
 CREATE TABLE relateditems (
@@ -24,11 +24,19 @@ CREATE TABLE relateditems (
     FOREIGN KEY (product_id) REFERENCES product (product_id)
 );
 
+CREATE TABLE features (
+  id SERIAL PRIMARY KEY NOT NULL,
+  product_id INT NOT NULL,
+  feature VARCHAR(30) NOT NULL,
+  feature_value VARCHAR(30) NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES product (product_id)
+);
+
 CREATE TABLE styles (
   id SERIAL PRIMARY KEY NOT NULL,
   product_id INT NOT NULL,
-  style_name VARCHAR(10),
-  sale_price INT NOT NULL,
+  style_name VARCHAR(50),
+  sale_price VARCHAR(4),
   original_price INT NOT NULL,
   default_style BOOLEAN NOT NULL,
   FOREIGN KEY (product_id) REFERENCES product (product_id)
@@ -37,23 +45,31 @@ CREATE TABLE styles (
 CREATE TABLE photos (
   id SERIAL PRIMARY KEY NOT NULL,
   style_id INT NOT NULL,
-  pic_url VARCHAR(200) NOT NULL,
-  thumbnail_url VARCHAR(200) NOT NULL,
+  pic_url VARCHAR(250) NOT NULL,
+  thumbnail_url TEXT NOT NULL,
     FOREIGN KEY (style_id) REFERENCES styles (id)
 );
 
 CREATE TABLE inventory (
   id SERIAL PRIMARY KEY NOT NULL,
   style_id INT NOT NULL,
-  size CHAR(3) NOT NULL,
+  size VARCHAR(10) NOT NULL,
   quantity INT NOT NULL,
     FOREIGN KEY (style_id) REFERENCES styles (id)
 );
 
-CREATE TABLE features (
-  id SERIAL PRIMARY KEY NOT NULL,
-  product_id INT NOT NULL,
-  feature VARCHAR(30) NOT NULL,
-  feature_value VARCHAR(30) NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES product (id)
-);
+
+
+/***                                CSV COMMANDS                               ***/
+
+-- \COPY product(product_id, item_name, slogan, product_description, category, default_price) FROM '/Users/ray/Desktop/HackReactor/SDC Application Data Sets/ProductsData/product.csv' DELIMITER ',' CSV HEADER;
+
+-- \COPY relateditems(id, product_id, related_id) FROM '/Users/ray/Desktop/HackReactor/SDC Application Data Sets/ProductsData/related.csv' DELIMITER ',' CSV HEADER;
+
+-- \COPY features(id, product_id, feature, feature_value) FROM '/Users/ray/Desktop/HackReactor/SDC Application Data Sets/ProductsData/features.csv' DELIMITER ',' CSV HEADER;
+
+-- \COPY styles(id, product_id, style_name, sale_price, original_price, default_style) FROM '/Users/ray/Desktop/HackReactor/SDC Application Data Sets/ProductsData/styles.csv' DELIMITER ',' CSV HEADER;
+
+-- \COPY photos(id, style_id, pic_url, thumbnail_url) FROM '/Users/ray/Desktop/HackReactor/SDC Application Data Sets/ProductsData/photos.csv' DELIMITER ',' CSV HEADER;
+
+-- \COPY inventory(id, style_id, size, quantity) FROM '/Users/ray/Desktop/HackReactor/SDC Application Data Sets/ProductsData/skus.csv' DELIMITER ',' CSV HEADER;
